24851c5d7f5da8e664b691ef26f56e36972a3662
HeRaNO <heran55@126.com>
2023-11-24 13:05:04 +0800

    refact submission download tool with async

--- ICPCToy/DOMjudge/README.md
+++ ICPCToy/DOMjudge/README.md
@@ -2,5 +2,5 @@
 
 与 DOMjudge 有关的乱七八糟的东西。
 
-- `getDOMjudgeSubmissions.py`：从 DOMjudge 上获取比赛的所有提交。
+- `dj_download_submissions.py`：从 DOMjudge 上获取某场比赛的所有提交。注意先安装 `requirements.txt` 中的依赖。
 - `gen_pwd.py`：生成随机密码（`0-9a-z`）。
---
+++ ICPCToy/DOMjudge/dj_download_submissions.py
@@ -0,0 +1,61 @@
+import os
+import aiohttp
+import asyncio
+import aiofiles
+import argparse
+from getpass import getpass
+from base64 import b64decode
+from tqdm.asyncio import tqdm
+
+parser = argparse.ArgumentParser(description='Download all submissions from DOMjudge.')
+parser.add_argument('url', type=str, help='DOMjudge url. Example: http://localhost/domjudge')
+parser.add_argument('cid', type=int, help='Contest ID.')
+parser.add_argument('-u', '--username', type=str, default='admin', help='DOMjudge admin username. Default: admin')
+args = parser.parse_args()
+
+password = getpass(f'[*] Input the password of user {args.username}: ')
+
+submission_url = f'{args.url}/api/v4/contests/{args.cid}/submissions'
+auth_info = aiohttp.BasicAuth(args.username, password)
+root_path = 'submissions'
+
+async def get_submissions():
+	async with aiohttp.ClientSession() as session:
+		async with session.get(submission_url, auth=auth_info) as resp:
+			resp.raise_for_status()
+			return await resp.json()
+
+async def write_file(file, content):
+	async with aiofiles.open(file, mode='wb') as f:
+		await f.write(content)
+
+sub_list = asyncio.run(get_submissions())
+all_submissions_cnt = len(sub_list)
+print(f'{all_submissions_cnt} submission(s) found.')
+
+if not os.path.exists(root_path):
+	os.mkdir(root_path)
+
+async def main_downloading():
+	async with aiohttp.ClientSession() as session:
+		async def download_one(rec):
+			sub_id = rec['id']
+
+			code_path = os.path.join(root_path, rec['problem_id'])
+			if not os.path.exists(code_path):
+				os.mkdir(code_path)
+
+			file_url = f'{submission_url}/{sub_id}/source-code'
+			async with session.get(file_url, auth=auth_info) as resp:
+				resp.raise_for_status()
+				files = await resp.json()
+
+			file = files[0]
+			file_name = '-'.join([rec['team_id'], sub_id, file['filename']])
+			file_path = os.path.join(code_path, file_name)
+			await write_file(file_path, b64decode(file['source']))
+
+		tasks = [download_one(rec) for rec in sub_list]
+		await tqdm.gather(*tasks)
+
+asyncio.run(main_downloading())
