cc865d86e89ae1c778dbf28e953348c7d87d39ee
HeRaNO <heran55@126.com>
2024-06-16 20:12:35 +0800

    update make_json.py

--- ICPCToy/SCCPC/make_json.py
+++ ICPCToy/SCCPC/make_json.py
@@ -1,12 +1,21 @@
 import json
 import random
 
-all_teams = open('all_teams.csv', encoding='utf-8')
+all_team = open('do_tsv.tsv', encoding='utf-8')
+
+random.seed()
+
+pinyin = []
+
+with open('pinyin-table.txt', 'r') as f:
+	for line in f:
+		pinyin.append(line.strip())
 
 group_id_dict = {
-	'正式队伍': '3',
-	'打星队伍': '4',
-	'女生队伍': '5'
+	'普通名额': 'participants',
+	'普通队伍': 'participants',
+	'打星队伍': 'observers',
+	'女生队伍': 'organization'
 }
 
 affiliations_id_dict = {}
@@ -21,19 +30,13 @@ accounts_warmup = []
 passwd_let_warmup = {}
 
 def gen_passwd(length):
-	x = random.randint(1, 36 ** length)
-	v = []
-	while x > 0:
-		t = x % 36
-		if t >= 10:
-			v.append(chr(t + 87))
-		else:
-			v.append(chr(t + 48))
-		x = x // 36
-	while len(v) < length:
-		v.append('0')
-	v.reverse()
-	return "".join(str(i) for i in v)
+	pwd_part = []
+	len_sum = 0
+	while len_sum < length:
+		t = random.choice(pinyin)
+		pwd_part.append(t)
+		len_sum += len(t)
+	return '-'.join(pwd_part)
 
 def get_group_id(info):
 	res = group_id_dict.get(info)
@@ -57,32 +60,33 @@ def get_affiliation_id(org):
 		affiliations_id += 1
 	return res
 
-for team in all_teams:
-	team_info = team.strip().split(',')
+for team in all_team:
+	team_info = team.strip().split('\t')
+	print(team_info)
 	teams.append({
-		'id': f'{team_id}',
+		'id': f'team_{team_id}',
 		'group_ids': [get_group_id(team_info[3])],
 		'name': team_info[2],
-		# 'members': make_members(team_info[4: 7]),
+		'members': make_members(team_info[4:7]),
 		'organization_id': f'org_{get_affiliation_id(team_info[1])}',
-		# 'room': team_info[7]
+		'room': team_info[7]
 	})
-	pwd = gen_passwd(8)
-	pwd_warmup = gen_passwd(8)
+	pwd = gen_passwd(10)
+	pwd_warmup = gen_passwd(10)
 	accounts.append({
-		'id': f'team_{team_id}',
+		'id': f'account_{team_id}',
 		'username': 'team%03d' % (team_id),
 		'password': pwd,
 		'type': 'team',
-		'team_id': f'{team_id + 2}',
+		'team_id': f'team_{team_id}',
 	})
 	passwd_let[team_info[7]] = [team_info[2], 'team%03d' % (team_id), pwd]
 	accounts_warmup.append({
-		'id': f'warmup_team_{team_id}',
+		'id': f'warmup_account_{team_id}',
 		'username': 'warmup_team%03d' % (team_id),
 		'password': pwd_warmup,
 		'type': 'team',
-		'team_id': f'{team_id + 2}',
+		'team_id': f'team_{team_id}',
 	})
 	passwd_let_warmup[team_info[7]] = [team_info[2], 'warmup_team%03d' % (team_id), pwd_warmup]
 	team_id += 1
@@ -110,11 +114,11 @@ with open('accounts_warmup.json', 'w', encoding='utf-8') as f:
 	f.write(json.dumps(accounts_warmup, ensure_ascii=False))
 
 with open('pwdlet.tsv', 'w', encoding='utf-8') as f:
-	for k in sorted(passwd_let.keys(), key=lambda x: int(x[: -1]) * 10 + ord(x[-1]) - ord('A')):
+	for k in sorted(passwd_let.keys(), key=lambda x: (ord(x[-1]) - ord('A') + 1) * 100 + int(x[: -1])):
 		f.write('\t'.join([k] + passwd_let[k]))
 		f.write('\n')
 
 with open('pwdlet_warmup.tsv', 'w', encoding='utf-8') as f:
-	for k in sorted(passwd_let_warmup.keys(), key=lambda x: int(x[: -1]) * 10 + ord(x[-1]) - ord('A')):
+	for k in sorted(passwd_let_warmup.keys(), key=lambda x: (ord(x[-1]) - ord('A') + 1) * 100 + int(x[: -1])):
 		f.write('\t'.join([k] + passwd_let_warmup[k]))
 		f.write('\n')
