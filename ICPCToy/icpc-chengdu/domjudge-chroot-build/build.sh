47a88e24f05edae24aec6ab7c3aa4c5728f09a0c
Heran Yang <heran55@126.com>
2024-10-28 16:54:35 +0800

    small fix

--- ICPCToy/icpc-chengdu/domjudge-chroot-build/build.sh
+++ ICPCToy/icpc-chengdu/domjudge-chroot-build/build.sh
@@ -2,4 +2,4 @@
 sudo ./dj_make_chroot -y -c "$(pwd)"/archives -d chroot \
   -s "$(ls $(pwd)/assets/*.list 2>/dev/null | tr '\n' ',')" \
   -m https://sysopspackages.icpc.global/ubuntu \
-  -D Ubuntu -R jammy -a amd64 && tar -C chroot -zcvf chroot.tar.gz .
+  -D Ubuntu -R jammy -a amd64 && sudo tar -C chroot -zcvf chroot.tar.gz .
